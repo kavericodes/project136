f65556033e224397729bd6e1a9b2094ddf618056
Kaveri <77133605+kavericodes@users.noreply.github.com>
2022-01-24 16:09:25 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(630,420);
+    canvas.center();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw(){
+    image(video,0,0,630,410)
+}
